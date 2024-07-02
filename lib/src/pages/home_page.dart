b9ceb88b46798ed863a3562535f59dab1b359f5f
KimDongWook <kimdw3824@gmail.com>
2024-07-02 16:48:15 +0900

    바텀앱바 작업 시작

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int pageIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: IndexedStack(
+        index: pageIndex,
+        children: [
+          const HomePage(),
+          Container(color: Colors.red),
+          Container(color: Colors.green),
+          Container(color: Colors.blue),
+        ],
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          children: [],
+        ),
+      ),
+    );
   }
 }
