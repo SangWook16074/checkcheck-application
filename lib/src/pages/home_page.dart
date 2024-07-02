f496d037c9c83b6fba4953d04fd915044a487904
KimDongWook <kimdw3824@gmail.com>
2024-07-02 17:30:23 +0900

    바텀 앱바 제작 중

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -8,13 +8,13 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int pageIndex = 0;
+  int _pageIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: IndexedStack(
-        index: pageIndex,
+        index: _pageIndex,
         children: [
           const HomePage(),
           Container(color: Colors.red),
@@ -22,10 +22,14 @@ class _HomePageState extends State<HomePage> {
           Container(color: Colors.blue),
         ],
       ),
-      bottomNavigationBar: BottomAppBar(
-        child: Row(
-          children: [],
-        ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "홈"),
+          BottomNavigationBarItem(icon: Icon(Icons.handshake), label: "수강신청"),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.check_circle), label: "출결현황"),
+          BottomNavigationBarItem(icon: Icon(Icons.settings), label: "마이페이지"),
+        ],
       ),
     );
   }
